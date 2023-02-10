6dd47351f9db12788510ae823b739df0f0d18c02
zox <fan_zhanhao@qq.com>
2023-02-10 19:55:54 +0800

    first commit

---
+++ open.sh
@@ -0,0 +1,1 @@
+nohup ./trojan-go -config config.json > ./output.txt 2>&1
---
+++ setup.sh
@@ -0,0 +1,7 @@
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/atrandys/trojan/master/trojan_mult.sh)"
+
+wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh"
+
+chmod +x tcp.sh
+
+./tcp.sh
